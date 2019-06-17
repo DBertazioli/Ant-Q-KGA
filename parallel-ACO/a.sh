d0d7f6fd9d3f120483c83fe2fdb2e115589245ae
DBertazioli <d.bertazioli@campus.unimib.it>
2019-06-17 18:04:18 +0200

    pr1002

--- parallel-ACO/a.sh
+++ parallel-ACO/a.sh
@@ -6,17 +6,18 @@
 for i in {0..0} #set n_repeat here
   do 
      #time mpirun --hostfile hostfile -n 2 python3 main.py test/pr76 76 30 1 3 0.5 $i
-     time mpirun -n 2 python3 main.py test/a280 280 100 1 3 0.5 $i #bad smth
+     #time mpirun -n 2 python3 main.py test/a280 280 100 1 3 0.5 $i #bad smth
      #time mpirun -n 2 python3 main.py test/ch130 130 30 1 3 0.5 $i
      #time mpirun -n 2 python3 main.py test/berlin52 52 1000 1 3 0.5 $i
      #echo "time mpirun -n 2 python3 main.py test/berlin52 52 1000 1 3 0.5 $i"
-     echo "time mpirun -n 2 python3 main.py test/a280 280 100 1 3 0.5 $i"
+     time mpirun -n 2 python3 main.py test/pr1002 1002 10 1 3 0.5 $i
+     echo "time mpirun -n 2 python3 main.py test/pr1002 1002 10 1 3 0.5 $i"
      #time mpirun -n 2 python3 main.py test/d198 198 30 1 3 0.5 $i
   
   
   #echo "\n\n****pr76****\n\n"
   #echo "\n\n****ch130****\n\n"
-  echo "\n\n****berlin52****\n\n"
+  echo "\n\n****pr1002****\n\n"
   #echo "\n\n****d198****\n\n"
  done
  
